7d0e428f8ba0dd393ca3dee40f96cb2e646aa842
deovrat-n <deovrat2203@gmail.com>
2024-07-17 23:31:57 +0530

    created new project

---
+++ myproject/snippets/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+
+    class Meta:
+        ordering = ['created']
+
+"""
+This model defines a Snippet object with fields for creation date, title, code, line numbers,
+ programming language, and style. The language and style fields use choices populated from 
+ the pygments library, which provides a comprehensive set of options for syntax highlighting.
+ The Meta class ensures that snippets are ordered by their creation date by default.
+"""
